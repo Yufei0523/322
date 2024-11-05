da3c7197bdfbb8dd7dbb147788f8a47efd0c8744
DECAVE Gabriel <gabriel.decave@telecomnancy.eu>
2024-11-05 18:54:30 +0000

    changed script.ts to script.js

--- Spotify_info_display/src/script.js
+++ Spotify_info_display/src/script.js
@@ -3,7 +3,9 @@ const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
 if (!code) {
+
     redirectToAuthCodeFlow(clientId);
+
 } else {
 
     // Loading of the Data
@@ -32,7 +34,7 @@ if (!code) {
 
 //// getAccessToken function ////
 
-export async function getAccessToken(clientId: string, code: string): Promise<string> {
+export async function getAccessToken(clientId, code) {
     const verifier = localStorage.getItem("verifier");
 
     const params = new URLSearchParams();
@@ -40,7 +42,7 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
     params.append("grant_type", "authorization_code");
     params.append("code", code);
     params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("code_verifier", verifier!);
+    params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
@@ -54,7 +56,7 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
 
 //////////// fetchProfile function ////////////
 
-async function fetchProfile(token: string): Promise<UserProfile> {
+async function fetchProfile(token) {
     const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
@@ -64,25 +66,25 @@ async function fetchProfile(token: string): Promise<UserProfile> {
 
 //////////// populateUI function ////////////
 
-function populateUI(profile: UserProfile) {
-    document.getElementById("displayName")!.innerText = profile.display_name;
+function populateUI(profile) {
+    document.getElementById("displayName").innerText = profile.display_name;
     if (profile.images[0]) {
         const profileImage = new Image(200, 200);
         profileImage.src = profile.images[0].url;
-        document.getElementById("avatar")!.appendChild(profileImage);
+        document.getElementById("avatar").appendChild(profileImage);
+        document.getElementById("imgUrl").innerText = profile.images[0].url;
     }
-    document.getElementById("id")!.innerText = profile.id;
-    document.getElementById("email")!.innerText = profile.email;
-    document.getElementById("uri")!.innerText = profile.uri;
-    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url")!.innerText = profile.href;
-    document.getElementById("url")!.setAttribute("href", profile.href);
-    document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+    document.getElementById("id").innerText = profile.id;
+    document.getElementById("email").innerText = profile.email;
+    document.getElementById("uri").innerText = profile.uri;
+    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url").innerText = profile.href;
+    document.getElementById("url").setAttribute("href", profile.href);
 }
 
 //////////// Recovery of Spotify data ////////////
 
-export async function redirectToAuthCodeFlow(clientId: string) {
+export async function redirectToAuthCodeFlow(clientId) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
 
@@ -99,7 +101,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
 
-function generateCodeVerifier(length: number) {
+function generateCodeVerifier(length) {
     let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -109,7 +111,7 @@ function generateCodeVerifier(length: number) {
     return text;
 }
 
-async function generateCodeChallenge(codeVerifier: string) {
+async function generateCodeChallenge(codeVerifier) {
     const data = new TextEncoder().encode(codeVerifier);
     const digest = await window.crypto.subtle.digest('SHA-256', data);
     return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
@@ -118,9 +120,9 @@ async function generateCodeChallenge(codeVerifier: string) {
         .replace(/=+$/, '');
 }
 
-//////////// CreatePlaylist function ////////////
+//////////// createPlaylist function ////////////
 
-async function createPlaylist(userID: string, accessToken: string) {
+async function createPlaylist(userID, accessToken) {
 
     // parameters of the playlist (can be changed by the user later)
 
@@ -135,7 +137,8 @@ async function createPlaylist(userID: string, accessToken: string) {
         method: "POST",
         headers: { 
             "Authorization": `Bearer ${accessToken}`,
-            "Content-Type": "application/json" },
+            "Content-Type": "application/json" 
+        },
         body: JSON.stringify(param)
     });
 
